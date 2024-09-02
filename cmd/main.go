d33e08b3891bd5f38b61404ced895928d03b25d0
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-09-02 21:01:32 +0500

    Add Database Connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"log"
 
+	"github.com/go-sql-driver/mysql"
+	"github.com/rajeshcskumar/ecom/cmd/api"
+	configs "github.com/rajeshcskumar/ecom/config"
+	"github.com/rajeshcskumar/ecom/db"
+)
+
+func main() {
+	cfg := mysql.Config{
+		User:                 configs.Envs.DBUser,
+		Passwd:               configs.Envs.DBPassword,
+		Addr:                 configs.Envs.DBAddress,
+		DBName:               configs.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	}
+	db, err := db.NewMySQLStorage(cfg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	initStorage(db)
+
+	server := api.NewAPIServer(":8000", db)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected!")
 }
